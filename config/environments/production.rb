26a8f4723d188bc218fe5193dc3c34ca935c6369
Ben Lavender <bhuga@github.com>
2015-08-26 18:51:14 +0900

    Don't digest assets until we figure it out

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,7 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
+  config.assets.digest = false 
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
